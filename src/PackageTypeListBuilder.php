afaee0f0b9246cff187909ed775732743fd7f285
jsacksick <jsacksick@972218.no-reply.drupal.org>
2017-01-17 18:38:16 +0100

    Issue #2841465 by jsacksick, bojanz: Use the number formatter in the PackageTypeListBuilder

--- src/PackageTypeListBuilder.php
+++ src/PackageTypeListBuilder.php
@@ -4,11 +4,49 @@ namespace Drupal\commerce_shipping;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\physical\NumberFormatterInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a listing of package types.
  */
 class PackageTypeListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * The number formatter.
+   *
+   * @var \Drupal\physical\NumberFormatterInterface
+   */
+  protected $numberFormatter;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('physical.number_formatter')
+    );
+  }
+
+  /**
+   * Constructs a new PackageTypeListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\physical\NumberFormatterInterface $number_formatter
+   *   The number formatter.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, NumberFormatterInterface $number_formatter) {
+    parent::__construct($entity_type, $storage);
+
+    $this->numberFormatter = $number_formatter;
+  }
 
   /**
    * {@inheritdoc}
@@ -27,17 +65,16 @@ class PackageTypeListBuilder extends ConfigEntityListBuilder {
     /** @var \Drupal\commerce_shipping\Entity\PackageTypeInterface $entity */
     $row['label'] = $entity->label();
 
-    // @todo Use the physical number formatter here.
     $dimensions = $entity->getDimensions();
     $dimension_list = [
-      $dimensions['length'],
-      $dimensions['width'],
-      $dimensions['height'],
+      $this->numberFormatter->format($dimensions['length']),
+      $this->numberFormatter->format($dimensions['width']),
+      $this->numberFormatter->format($dimensions['height']),
     ];
     $row['dimensions'] = implode(' × ', $dimension_list) . ' ' . $dimensions['unit'];
 
     $weight = $entity->getWeight();
-    $row['weight'] = $weight['number'] . ' ' . $weight['unit'];
+    $row['weight'] = $this->numberFormatter->format($weight['number']) . ' ' . $weight['unit'];
 
     return $row + parent::buildRow($entity);
   }
